e5640fdbd51978a95d5f7a1d929ee3befbae45f0
sosodefi <developer@sosodefi.com>
2020-08-29 23:07:16 +0800

    feat: complete footer

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 import Container from '../containers/Home';
 
 export default function Home() {
@@ -8,6 +9,7 @@ export default function Home() {
     <>
       <Header />
       <Container />
+      <Footer />
     </>
   );
 }
